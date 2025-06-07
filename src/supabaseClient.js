b3a3d25bcca85c5d87dfb317b808d7d7fa8587fe
rik williams <rik_williams@yahoo.com>
2025-06-07 15:07:19 +0100

    Fix: Use hardcoded Supabase values

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+//const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+//const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+const supabaseUrl ='https://elayyirzfehtdibdrpfv.supabase.co'
+
+const supabaseAnonKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsYXl5aXJ6ZmVodGRpYmRycGZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2OTY4OTEsImV4cCI6MjA1NzI3Mjg5MX0.mTZv-EGhlGA8bxyF_XTLhrCxV5GiA7phrdNjoRslpYs'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
